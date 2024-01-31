16b42a698906e49a076b8ebdaae8a364ca4f445f
tomkelly111 <thomaspkelly111@gmail.com>
2024-01-31 17:47:00 +0000

    update hello_world view with post method

--- hello_world/views.py
+++ hello_world/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world!")
+    if request.method == "POST":
+        HttpResponse("You must have POSTed something.")
+    else return HttpResponse(request.method)
